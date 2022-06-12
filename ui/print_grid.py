d534d2e47879009061e0c45cdf53c1a71fa4d980
Luke Scanlan <lukescanlanatwork@gmail.com>
2022-06-12 04:04:17 +0100

    Update print_grid to work with new data structures

--- ui/print_grid.py
+++ ui/print_grid.py
@@ -1,25 +1,26 @@
 
-from typing import Dict, List
-from logic2.get_groups import get_row
+from logic.get_groups import get_row
+from logic.types import BoxDimensions, Grid
 from ui.Colours import Colours
 
 
-def print_grid(grid: List[List[str]], box_dimensions: Dict[str, int]) -> None:
-    cell_num = box_dimensions["width"] * box_dimensions["height"]
+def print_grid(grid: Grid, box_dimensions: BoxDimensions) -> None:
+    width, height = box_dimensions["w"], box_dimensions["h"]
+    cell_num = width * height
     grid_str = "\n"
-    for i, row in enumerate([get_row(grid, j, box_dimensions) for j in range(len(grid))]):
-        if i != 0 and i % box_dimensions["height"] == 0:
-            grid_str += f"{((('——' if cell_num < 10 else '———') * box_dimensions['width'] + '—+') * box_dimensions['height'])[:-1]}\n"
+    for i, row in enumerate([get_row(grid, n) for n in range(len(grid))]):
+        if i != 0 and i % height == 0:
+            grid_str += f"{((('——' if cell_num < 10 else '———') * box_dimensions['w'] + '—+') * box_dimensions['h'])[:-1]}\n"
         for j, cell in enumerate(row):
-            if j != 0 and j % box_dimensions["width"] == 0:
+            if j != 0 and j % width == 0:
                 grid_str += " |"
             grid_str += f" {cell}" if cell_num < 10 or len(cell) > 1 else f" {cell} "
             if j + 1 == len(row):
                 grid_str += f"  {Colours.OKCYAN}{i + 1}{Colours.END}\n"
         if i + 1 == len(grid):
-            grid_str += ("  " if cell_num < 10 else "   " * box_dimensions["width"] + " |") * (box_dimensions["height"] - 1)
+            grid_str += ("  " if cell_num < 10 else "   " * width + " |") * (height - 1)
             col_indices = [f" {k}" if cell_num < 10 or k > 9 else f" {k} " for k in range(1, cell_num + 1)]
-            col_indices = [f"  {char}" if i != 0 and i % box_dimensions["width"] * 2 == 0 else char for i, char in enumerate(col_indices)]
+            col_indices = [f"  {char}" if i != 0 and i % width * 2 == 0 else char for i, char in enumerate(col_indices)]
             grid_str += f"\n{Colours.OKCYAN + ''.join(col_indices) + Colours.END}"
 
     print(grid_str)
