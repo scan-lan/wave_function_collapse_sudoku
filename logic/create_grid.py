014ac6e8e2196f5b47d2df524e5d3161b56ea791
Luke Scanlan <lukescanlanatwork@gmail.com>
2022-06-12 02:27:39 +0100

    Add create_coefficient_matrix

--- logic/create_grid.py
+++ logic/create_grid.py
@@ -1,45 +1,20 @@
-from typing import List
 from random import shuffle
-from logic.Grid import Grid
-from math import floor
+from logic.types import Grid, BoxDimensions, Coefficients, CoefficientMatrix
 
-def check_grid_valid(grid: Grid):
-    for i in range(1, len(grid.grid)):
-        for group in [grid.row, grid.col, grid.box]:
-            if len(set(group(i))) < len(grid.grid):
-                return False
-    return True
-
-# rows: List[List[int]] = [[0] * number_of_cells for _ in range(number_of_cells)]
 # rows = [[(j + (floor(i / 3)) + (i % 3) * 3) % 9 + 1 for j in range(9)] for i in range(9)]
 
-def create_test_grid() -> Grid:
-    rows = [[(j + (floor(i / 3)) + (i % 3) * 3) % 9 + 1 for j in range(9)] for i in range(9)]
-    return Grid(rows, rows, 3, 3)
+def create_coefficient_matrix(size: int) -> CoefficientMatrix:
+    coefficients: Coefficients = set([str(n) for n in range(1, size+1)])
+    coefficient_matrix: CoefficientMatrix = []
+    for _ in range(size):
+        row = [coefficients.copy() for _ in range(size)]
+        coefficient_matrix.append(row)
 
-def create_random_grid(box_width: int = 3, box_height: int = 3) -> Grid:
-    number_of_cells = box_width * box_height
-    rows: List[List[int]] = [[0] * number_of_cells for _ in range(number_of_cells)]
+    return coefficient_matrix
 
-    free_boxes = min([box_width, box_height])
-    for i in range(free_boxes):
-        values = [*range(1, number_of_cells + 1)]
-        shuffle(values)
-        for j in range(box_height):
-            values_index = j * box_width
-            rows[i * box_height + j][i*box_width:i*box_width+box_width] = values[values_index:values_index+box_width]
-    
-    for i in range(9):
-        for j in range(6):
-            pass
 
-    grid = Grid(rows, rows, box_width, box_height)
-
-    return grid
-
-def create_grid(box_width: int = 3, box_height: int = 3, difficulty: int = 1) -> Grid:
-    grid = create_random_grid(box_width, box_height)
-    # while not check_grid_valid(grid):
-    #     grid = create_random_grid(box_width, box_height)
-    
-    return grid
+def create_grid(box_dimensions: BoxDimensions = {"w": 3, "h": 3}, difficulty: int = 1) -> Grid:
+    grid_size = box_dimensions["w"] * box_dimensions["h"]
+    # solution_grid = [["0"] * grid_size for _ in range(grid_size)]
+    coefficient_matrix = create_coefficient_matrix(grid_size)
+    return [[]]
