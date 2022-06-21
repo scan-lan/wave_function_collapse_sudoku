1944d77dfacd445c949324aa39560cd6acea83ee
Luke Scanlan <luke@lukescanlan.com>
2022-06-21 14:21:15 +0100

    Replace bodged test skipping logic with pytest select

--- Makefile
+++ Makefile
@@ -12,16 +12,16 @@ lint-fix:
 	autopep8 -iraa main.py logic ui util --max-line-length 120
 
 test:
-	pytest -s
+	pytest -s -m "not performance and not success_rate"
 
-BENCHMARK_COLUMNS:=$(shell echo "'mean, min, ops, iqr, rounds, iterations'")
-BENCHMARK_SORT:=$(shell echo "'name'")
+BENCHMARK_COLUMNS:=$(shell echo "'mean, min, ops, rounds, iterations'")
+BENCHMARK_SORT:=$(shell echo "'mean'")
 
 test-performance:
-	pytest --run-performance --benchmark-autosave --benchmark-columns=${BENCHMARK_COLUMNS} --benchmark-sort=${BENCHMARK_SORT}
+	pytest -m performance --benchmark-autosave --benchmark-columns=${BENCHMARK_COLUMNS} --benchmark-sort=${BENCHMARK_SORT}
 
 test-performance-no-save:
-	pytest -s --run-performance --benchmark-only --benchmark-columns=${BENCHMARK_COLUMNS} --benchmark-sort=${BENCHMARK_SORT}
+	pytest -s -m performance --benchmark-only --benchmark-columns=${BENCHMARK_COLUMNS} --benchmark-sort=${BENCHMARK_SORT}
 
 RUN_ONE:=$(shell run_one=$$(ls -rt .benchmarks/**/*.json | tail -n 2 | head -n 1) && run_one=$${run_one##*/} && echo $${run_one:0:4})
 RUN_TWO:=$(shell run_two=$$(ls -rt .benchmarks/**/*.json | tail -n 1) && run_two=$${run_two##*/} && echo $${run_two:0:4})
@@ -29,7 +29,7 @@ compare-last-two-performances:
 	pytest-benchmark compare ${RUN_ONE} ${RUN_TWO} --columns=${BENCHMARK_COLUMNS} --benchmark-sort=${BENCHMARK_SORT}
 
 test-success-rates:
-	pytest --run-success-rate
+	pytest -m success_rate
 
 run: requirements-base
 	python3 main.py
--- conftest.py
+++ conftest.py
@@ -1,38 +1,6 @@
-from typing import Iterable, Literal
 import pytest
-
-
-def pytest_addoption(parser: pytest.Parser):
-    parser.addoption("--run-success-rate", action="store_true", default=False,
-                     help="run tests showing success rate of grid generation")
-    parser.addoption("--run-performance", action="store_true", default=False,
-                     help="run performance tests")
 
 
 def pytest_configure(config: pytest.Config):
     config.addinivalue_line("markers", "success_rate: mark test as a success rate test")
     config.addinivalue_line("markers", "performance: mark test as a performance test")
-
-
-def pytest_collection_modifyitems(config: pytest.Config, items: Iterable[pytest.Item]):
-    run_success_rate = config.getoption("--run-success-rate")
-    run_performance = config.getoption("--run-performance")
-    run_type_markers = {"success_rate": {True: pytest.mark.skip(reason="used option --run-success-rate"),
-                                         False: pytest.mark.skip(reason="need --run-success-rate option to run")},
-                        "performance": {True: pytest.mark.skip(reason="used option --run-performance"),
-                                        False: pytest.mark.skip(reason="need --run-performance option to run")}}
-    run_type: Literal["success_rate", "performance", None] = None
-    if run_performance:
-        run_type = "performance"
-    elif run_success_rate:
-        run_type = "success_rate"
-
-    if run_type is None:
-        for item in items:
-            for run_type_key in run_type_markers.keys():
-                if run_type_key in item.keywords:
-                    item.add_marker(run_type_markers[run_type_key][False])
-    else:
-        for item in items:
-            if run_type not in item.keywords:
-                item.add_marker(run_type_markers[run_type][True])
