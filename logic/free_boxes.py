06dd4e74363adbe167e405df4908cf795867e735
Luke Scanlan <luke@lukescanlan.com>
2022-06-22 00:08:51 +0100

    Add logic from get_all_constraints to fill_free_boxes
    This did not improve performance

--- logic/free_boxes.py
+++ logic/free_boxes.py
@@ -3,7 +3,7 @@ from typing import Optional
 from logic.get_groups import get_coords_in_box
 
 from logic.types import Cell, CoefficientMatrix, Collapsed, Coords, Dimensions, Weights
-from logic.wave_function import collapse, propagate
+from logic.wave_function import collapse, constrain_all, get_collapsed_value, propagate
 from ui.print_coef_matrix import print_coef_matrix
 
 
@@ -52,21 +52,28 @@ def fill_free_boxes(
     box_size = box_dimensions["h"] * box_dimensions["w"]
     values = get_random_values(box_size)
     collapsed.update(free_cell_coords)
-    for coords in free_cell_coords:
+    for free_coords in free_cell_coords:
         if len(values) == 0:
             values = get_random_values(box_size)
-        collapse(coef_matrix, coords, weights, collapsed, value=values.pop())
+        collapse(coef_matrix, free_coords, weights, collapsed, value=values.pop())
         if visualise:
             print_coef_matrix(
-                coef_matrix, box_dimensions, sleep=(2 / speed), new_collapse=coords
+                coef_matrix, box_dimensions, sleep=(2 / speed), new_collapse=free_coords
             )
-        propagate(
-            coef_matrix,
-            box_dimensions,
-            coords,
-            weights,
-            collapsed,
-            skip=["box"],
-            visualise=visualise,
-            speed=speed,
-        )
+    for coords in [
+        (y, x)
+        for y in range(box_size)
+        for x in range(box_size)
+        if (y, x) not in collapsed
+    ]:
+        y, x = coords
+        constrain_all(coef_matrix, box_dimensions, coords)
+        if len(coef_matrix[y][x]) == 1:
+            collapse(
+                coef_matrix,
+                coords,
+                weights,
+                collapsed,
+                value=get_collapsed_value(coef_matrix[y][x]),
+            )
+            propagate(coef_matrix, box_dimensions, coords, weights, collapsed)
