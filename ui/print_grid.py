ffe2de08dbf17be17b871da2405489012127f9ce
Luke Scanlan <luke@lukescanlan.com>
2022-06-22 11:55:45 +0100

    Update ui methods to use dict matrix

--- ui/print_grid.py
+++ ui/print_grid.py
@@ -1,30 +1,30 @@
-from logic.get_groups import get_row
-from logic.types import Dimensions, Grid
 from colorama import init, Fore
+
+from logic.types import Dimensions, Grid
 
 init()
 
 
 def print_grid(grid: Grid, box_dimensions: Dimensions) -> None:
     width, height = box_dimensions["w"], box_dimensions["h"]
-    cell_num = width * height
-    str_length = len(f"{cell_num}")
+    size = width * height
+    str_length = len(f"{size}")
     grid_str = "\n"
-    for i, row in enumerate([get_row(grid, n) for n in range(len(grid))]):
-        if i != 0 and i % height == 0:
+    for y in range(size):
+        if y != 0 and y % height == 0:
             box_bottom = ("—" * (str_length + 1)) * box_dimensions["w"] + "—+"
             grid_str += (box_bottom * box_dimensions["h"])[:-1] + "\n"
-        for j, cell in enumerate(row):
-            if j != 0 and j % width == 0:
+        for x in range(size):
+            cell = grid[f"{y}, {x}"]
+            if x != 0 and x % width == 0:
                 grid_str += " |"
-            grid_str += f" {cell}" if cell_num < 10 or len(cell) > 1 else f" {cell} "
-            if j + 1 == len(row):
-                grid_str += f"  {Fore.CYAN}{i + 1}{Fore.RESET}\n"
-        if i + 1 == len(grid):
-            grid_str += ("  " if cell_num < 10 else "   " * width + " |") * (height - 1)
+            grid_str += f" {cell}" if size < 10 or str_length > 1 else f" {cell} "
+            if x + 1 == size:
+                grid_str += f"  {Fore.CYAN}{y + 1}{Fore.RESET}\n"
+        if y + 1 == size:
+            grid_str += ((" " * (str_length + 1)) * width + " |") * (height - 1)
             col_indices = [
-                f" {k}" if cell_num < 10 or k > 9 else f" {k} "
-                for k in range(1, cell_num + 1)
+                f" {k}" if size < 10 or k > 9 else f" {k} " for k in range(1, size + 1)
             ]
             col_indices = [
                 f"  {char}" if i != 0 and i % width * 2 == 0 else char
