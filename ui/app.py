a030165392ff498986b3e56f6af711dfc7dadab6
Luke Scanlan <luke@lukescanlan.com>
2022-06-28 16:45:33 +0100

    Move centre_y, x to separate module

--- ui/app.py
+++ ui/app.py
@@ -1,30 +1,8 @@
 import curses
 from typing import Iterable, Optional
+from ui.align import centre_x, centre_y
 
 from ui.types import Justify, MenuEntry, MenuName, Quit, Window
-
-
-def centre_x(
-    width: int, string: Optional[str] = None, str_len: Optional[int] = None
-) -> int:
-    if string is not None:
-        return width // 2 - len(string) // 2
-    elif str_len is not None:
-        return width // 2 - str_len // 2
-    else:
-        return width // 2
-
-
-def centre_y(
-    height: int, string: Optional[str] = None, num_lines: Optional[int] = None
-) -> int:
-    if num_lines is not None:
-        return height // 2 - num_lines
-    elif string is not None:
-        num_lines = len(string.splitlines())
-        return height // 2 - num_lines
-    else:
-        return height // 2
 
 
 def add_quit_message(
