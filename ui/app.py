49e92635bf9b645e2698c99d5bcc9e21a0c62f8e
Luke Scanlan <luke@lukescanlan.com>
2022-07-05 08:01:28 +0100

    Extract add_title and add_status_message to module

--- ui/app.py
+++ ui/app.py
@@ -1,48 +1,9 @@
 import curses
-from typing import Iterable, Optional
+from typing import Optional
+
+from ui.add_components import add_status_message, add_title
 from ui.align import centre_x, centre_y
-
-from ui.types import Justify, MenuEntry, MenuName, Quit, Window
-
-
-def add_quit_message(
-    screen: Window, quit_message: str, justify: Justify = "left"
-) -> None:
-    start_x = 0
-    if justify == "right":
-        start_x = curses.COLS - 1 - len(quit_message)
-    screen.attron(curses.A_REVERSE)
-    screen.addstr(curses.LINES - 1, start_x, quit_message, curses.A_REVERSE)
-    screen.attroff(curses.A_REVERSE)
-    screen.noutrefresh()
-
-
-def add_title(
-    screen: Window,
-    title_str: str,
-    tagline_str: Optional[str] = None,
-    pad_args: Optional[Iterable[int]] = None,
-    start_y: Optional[int] = None,
-    start_x: Optional[int] = None,
-):
-    title_string_length = max([len(line) for line in title_str.splitlines()])
-    num_lines_vertical = len(title_str.splitlines())
-    if start_y is None:
-        start_y = centre_y(curses.LINES, num_lines=num_lines_vertical)
-    if start_x is None:
-        start_x = centre_x(curses.COLS, str_len=title_string_length)
-
-    for i, line in enumerate(title_str.splitlines()):
-        screen.addstr(start_y + i, start_x, line)
-
-    if tagline_str is not None:
-        tagline_start_x = centre_x(curses.COLS, tagline_str)
-        tagline_start_y = start_y + num_lines_vertical + 2
-        screen.addstr(tagline_start_y, tagline_start_x, tagline_str)
-    if pad_args is None:
-        screen.noutrefresh()
-    else:
-        screen.noutrefresh(0, 0, 0, 0, 22, 79)
+from ui.types import MenuEntry, MenuName, Quit, Window
 
 
 def splash_screen(screen: Window, splash_time: int, title: str) -> None:
@@ -104,7 +65,7 @@ def start_menu(screen: Window, title: str) -> MenuName | Quit:
             pass
         screen.addstr(0, 0, k)
         add_title(screen, title, start_y=3)
-        add_quit_message(screen, quit_message)
+        add_status_message(screen, quit_message)
         add_menu_options(screen, menus, highlighted=highlighted)
         curses.doupdate()
     return selected
@@ -192,7 +153,7 @@ __\\/\\\\\\///\\\\\\___\\/\\\\\\___\\/\\\\\\_
     end(screen)
 
 
-def end(screen: "curses._CursesWindow"):
+def end(screen: Window):
     curses.nocbreak()
     screen.keypad(False)
     curses.echo()
