6fcecc129d8212039fe3b75061d32cdcbffc4c7d
Luke Scanlan <luke@lukescanlan.com>
2022-07-08 15:07:53 +0100

    Add types for new visualise function

--- logic/types.py
+++ logic/types.py
@@ -1,4 +1,5 @@
-from typing import Literal, TypeAlias, TypeVar
+from typing import Callable, Literal, Optional, TypeAlias, TypeVar
+from typing_extensions import Protocol
 
 DimensionsKey: TypeAlias = Literal["w", "h"]
 Dimensions: TypeAlias = dict[DimensionsKey, int]
@@ -24,5 +25,17 @@ HistoryEntry: TypeAlias = tuple[Collapsed, Weights, CoefficientMatrix]
 History: TypeAlias = list[HistoryEntry]
 
 
+class Visualiser(Protocol):
+    def __call__(
+        self,
+        coef_matrix: CoefficientMatrix,
+        box_dimensions: Dimensions,
+        new_collapse: Optional[Coords] = None,
+        constraint_coords: Optional[Coords] = None,
+        target_coords: Optional[Coords] = None,
+        constraint_value: Optional[Cell] = None,
+    ) -> bool:
+        ...
 
 
+OuterVisualiser: TypeAlias = Callable[[CoefficientMatrix, Dimensions], Visualiser]
